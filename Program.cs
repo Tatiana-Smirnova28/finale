40c6ddec5c5aa9733fbaf91ba08f9280c5921632
Tatiana-Smirnova28 <123449658+Tatiana-Smirnova28@users.noreply.github.com>
2023-03-07 02:44:05 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,41 @@
-﻿// task 1
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int len = number.Length;
+﻿// 66
+Console.Write("Введите начальное число M:");
+int M = Int32.Parse(Console.ReadLine());
 
-if (len == 5)
+Console.Write("Введите начальное число N:");
+int N = Int32.Parse(Console.ReadLine());
+
+void GapBtwSum (int M, int N, int sum)
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {Console.WriteLine("да");}
-    else
-    {Console.WriteLine("нет");}
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    GapBtwSum(M, N, sum);
 }
-else
-{Console.WriteLine("введите пятизначное число");}
-// task 2
 
-Console.Clear();
+GapBtwSum(M, N, 0);
 
-Console.Write("Введите координату X(1): ");
-double x1 = Convert.ToDouble(Console.ReadLine());
+//68
+int m = Inputs("M: ");
+int n = Inputs("N: ");
 
-Console.Write("Введите координату Y(1): ");
-double y1 = Convert.ToDouble(Console.ReadLine());
+int functionAkkerman = Ack(m, n);
 
-Console.Write("Введите координату Z(1): ");
-double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
 
-Console.Write("Введите координату X(2): ");
-double x2 = Convert.ToDouble(Console.ReadLine());
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
 
-Console.Write("Введите координату Y(2): ");
-double y2 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Введите координату Z(2): ");
-double z2 = Convert.ToDouble(Console.ReadLine());
-
-double S = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine(Math.Round(S, 2));
-
-// Task 3
-
-Console.Clear();
-
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= n; i++)
-  Console.WriteLine($"{i} ^ 3 = {i * i * i} ");
+int Inputs(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
